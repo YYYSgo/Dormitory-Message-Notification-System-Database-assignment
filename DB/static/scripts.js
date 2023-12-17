07157605fd8cb50dabb049118d1ee1e651b47def
yyys <102903455+YYYSgo@users.noreply.github.com>
2023-12-17 13:11:17 +0800

    delete

--- DB/static/scripts.js
+++
@@ -1,5 +0,0 @@
-// Example JavaScript functionality
-document.addEventListener('DOMContentLoaded', function () {
-    // Add any interactive features or event listeners here
-    console.log('Script loaded.');
-});
--- README.md
+++
@@ -1,3 +0,0 @@
-# Dormitory-Message-Notification-System-Database-assignment
-为实现我们的数据库实验大作业，我们做了一个寝室消息管理系统。
-
